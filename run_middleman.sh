75f1fbc31496ae7a38ac54a73655aadb4f288bca
Makoto Abe <abe00makoto+git@gmail.com>
2014-11-08 22:03:27 +0900

    when blowser reload ,load source

--- run_middleman.sh
+++ run_middleman.sh
@@ -6,4 +6,4 @@ if [[ $OSTYPE != "darwin"* ]]; then
 fi
 
 docker build -t middleman4dm .
-docker run -p 4567:4567 -v "$(pwd)"/DiscoverMeteor_Jp:/work/DiscoverMeteorStatic/source/chapters/jp middleman4dm middleman server
+docker run -p 4567:4567 -p 35729:35729 -v "$(pwd)"/DiscoverMeteor_Jp:/work/DiscoverMeteorStatic/source/chapters/jp middleman4dm middleman server --force-polling
